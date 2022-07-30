82ee08ae3b1e61dd16f3176a03cca31dbbf447c9
Cwarrick10 <warrickchar@gmail.com>
2022-07-29 22:36:55 -0400

    start bin

---
+++ src/main/java/test/DiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+import static org.junit.Assert;
+
+
+public class DiceTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
